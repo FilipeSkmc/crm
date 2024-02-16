a650957cbf2870c10af72cf063883e244f540a94
FilipeSkmc <filipe-santana@outlook.com>
2024-02-16 20:55:29 -0300

    refact: nova forma de recuperar os enums

--- app/Http/Controllers/EnumController.php
+++ app/Http/Controllers/EnumController.php
@@ -2,18 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\Source;
-use App\Enums\Status;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class EnumController extends Controller
 {
     public function index(): JsonResponse
     {
         $enums = [];
-        $enums['lead-status'] = Status::all();
-        $enums['lead-source'] = Source::all();
+
+        $directory = app_path('Enums');
+
+        if (File::isDirectory($directory)) {
+            $files = File::files($directory);
+
+            foreach ($files as $file) {
+                $fileName = str_replace('.php', '', $file->getFilename());
+
+                $className = "App\Enums\\" . $fileName;
+
+                if (enum_exists($className)) {
+                    $enums[strtolower($fileName)] = $className::all();
+                }
+            }
+        }
 
         return $this->sendResponse($enums);
     }
